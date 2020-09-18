957fb1b1b35c8cd2bc695f452800f8706523b37b
Lausius <skinkens_email@yahoo.dk>
2020-09-18 13:11:23 +0200

    generic addition

--- MathCalculator/Program.cs
+++ MathCalculator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace MathCalculator
 {
@@ -6,7 +9,31 @@ namespace MathCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Client.Start();
+        }
+    }
+
+    class Client
+    {
+        public static void Start()
+        {
+            TcpClient clientSocket = new TcpClient("localhost", 30001);
+
+            using (clientSocket)
+            {
+                Console.WriteLine("Server found");
+                Stream ns = clientSocket.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true;
+
+                string mathExpression = Console.ReadLine();
+                sw.WriteLine(mathExpression);
+                var result = sr.ReadLine();
+                Console.WriteLine(result);
+                Console.ReadLine();
+                //Console.WriteLine(mathExpression + " = " + result);
+            }
         }
     }
 }
---
+++ MathServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MathServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Start();
+        }
+    }
+
+    class Server
+    {
+        public static void Start()
+        {
+            TcpListener serverSocket = new TcpListener(IPAddress.Loopback, 30001);
+            serverSocket.Start();
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Server Activated");
+            using (connectionSocket)
+            {
+                Stream ns = connectionSocket.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true;
+                var message = sr.ReadLine();
+                Console.WriteLine(message);
+                string[] numberArray = message.Split(" ");
+                double firstNumber = Convert.ToDouble(numberArray[0]);
+                double secondNumber = Convert.ToDouble(numberArray[2]);
+                double result = firstNumber + secondNumber;
+                sw.WriteLine(message + " = " + result);
+            }
+        }
+    }
+}
